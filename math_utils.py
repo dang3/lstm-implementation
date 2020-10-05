ab0edb68e4660b089b4f496d4b3bc83dc73b3f80
Dennis <dang.dennis21@gmail.com>
2020-10-05 15:55:11 -0700

    Added helper methods, added loss functions

--- math_utils.py
+++ math_utils.py
@@ -1,4 +1,21 @@
 import numpy as np
+from math import log
 
 def sigmoid(x):
     return 1.0/(1 + np.exp(-x))
+
+def dsigmoid(x):
+    return x * (1 - x)
+
+def tanh(x):
+    return np.tanh(x)
+
+def dtanh(x)
+    return 1 - (x*x)
+
+def mean_absolute_error(actual_output, label):
+    return abs(actual_output - label)
+
+def binary_cross_entropy(actual_output, label):
+    return -(label * log(actual_output) + (1 - label) * log(1 - actual_output))
+
